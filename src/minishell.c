baad6ee327a4d957469a4bc0b74783efb84bff0d
Santito <santito@santito.es>
2024-01-20 13:29:55 +0100

    print binary

--- src/minishell.c
+++ src/minishell.c
@@ -6,14 +6,18 @@
 /*   By: mpovill- <mpovill-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 12:11:32 by mpovill-          #+#    #+#             */
-/*   Updated: 2023/12/19 12:20:50 by mpovill-         ###   ########.fr       */
+/*   Updated: 2024/01/20 13:28:28 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	write(1, "hello shell\n", 13);
+	char *binary;
+	(void)argc;
+	binary = *argv;
+	while(*binary)
+		write(1, binary++, 1);
 	return (0);
 }
