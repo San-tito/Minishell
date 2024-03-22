d5abbb2c95c90dd12e6496859cddab453abb46ed
mboola <mpovilleti@gmail.com>
2024-03-22 13:54:49 +0100

    Started implementing the testing execution.

--- testing/tester.sh
+++ testing/tester.sh
@@ -6,11 +6,13 @@ lexer_tester="lexer_tester"
 parser_tester="parser_tester"
 executor_tester="executor_tester"
 expansor_tester="expansor_tester"
+outfile="tester_results"
+correct_outfile="results"
 
 # First make the makefile
 if [ -f ${makefile} ]
 then
-    make
+    make  &> /dev/null
 else
     echo "ERROR: Makefile not found!"
     exit 1
@@ -18,4 +20,26 @@ fi
 
 # ...Here in theory we should have all the executables
 # Execute the lexer tests
-# ./${lexer_tester} "test1" "outfile"
+if [ -f ${outfile} ]
+then
+    rm ${outfile}
+fi
+touch ${outfile}
+./${lexer_tester} "test1" ${outfile}
+./${lexer_tester} "test2" ${outfile}
+./${lexer_tester} "test2" ${outfile}
+./${lexer_tester} "test2" ${outfile}
+./${lexer_tester} "test2" ${outfile}
+./${lexer_tester} "test2" ${outfile}
+./${lexer_tester} "test2" ${outfile}
+
+diff ${correct_outfile} ${outfile} &> /dev/null
+if [ ! $? -eq 0 ]
+then
+    echo "Failed lexer tests."
+    exit 1
+else
+    echo "Passed lexer tests."
+fi
+
+rm ${outfile}
--- testing/tests/lexer_tester.c
+++ testing/tests/lexer_tester.c
@@ -10,13 +10,13 @@ int	main(int argc, char **argv)
 
 	if (argc != 3 && argv[2][0] != '\0')
 		return (1);
-	fd = open(argv[2], O_WRONLY | O_CREAT);
+	fd = open(argv[2], O_WRONLY | O_APPEND | O_CREAT);
 	if (fd == -1)
 		return (2);
 	if (lexer(argv[2]))
-		dprintf(fd, "Correct\n.");
+		dprintf(fd, "Correct.\n");
 	else
-		dprintf(fd, "Incorrect\n.");
+		dprintf(fd, "Incorrect.\n");
 	close(fd);
 	return (0);
 }
