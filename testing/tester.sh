0fd97f95169ba8e4bfbdc463384148dc910f57b8
mboola <mpovilleti@gmail.com>
2024-03-22 12:51:30 +0100

    Started testing implementation

---
+++ testing/tester.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Variables
+makefile="Makefile"
+lexer_tester="lexer_tester"
+parser_tester="parser_tester"
+executor_tester="executor_tester"
+
+# First make the makefile
+if [ -f ${makefile} ]; then;
+    make testers
+else
+    echo "ERROR: Makefile not found!"
+    exit 1
+fi
+
+# ...Here in theory we should have all the executables
+# Execute the lexer tests
+./${lexer_tester} "test1" "outfile"
---
+++ testing/tests/lexer_tester.c
@@ -0,0 +1,23 @@
+
+#include "../../include/minishell.h"
+#include <stdio.h>
+
+/*
+ *	Input must be: ./lexer_tester "str_to_test" "outfile"
+ */
+int	main(int argc, char **argv)
+{
+	int fd;
+
+	if (argc != 3 && argv[2][0] != '\0')
+		return (1);
+	fd = open(argv[2], O_WRONLY | O_CREAT);
+	if (fd == -1)
+		return (2);
+	if (lexer(argv[2]))
+		fprintf(fd, "Correct\n.");
+	else
+		fprintf(fd, "Incorrect\n.");
+	close(fd);
+	return (0);
+}
