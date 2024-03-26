598dcef7484a70442d188f5e49ac590d460c451a
mboola <mpovilleti@gmail.com>
2024-03-26 22:01:44 +0100

    Expanded the tests for the lexer

--- testing/tests/lexer_tester.c
+++ testing/tests/lexer_tester.c
@@ -21,6 +21,23 @@ int	main(int argc, char **argv)
 	fd = open(argv[1], O_WRONLY | O_APPEND | O_CREAT);
 	if (fd == -1)
 		return (2);
+	//Valid tests.
+	test_lexer(fd, "echo");
+	test_lexer(fd, "\"\"");
+	test_lexer(fd, "\"\'\'\"");
+	test_lexer(fd, "echo \"test1\"\'\"\'");
+	test_lexer(fd, "echo world \"\"");
+	test_lexer(fd, "echo \"(world)()()(((()))))))))))\"");
+	test_lexer(fd, "echo \"asc 1 123  	 123		2313	 \' aa   ((((	\n		\"");
+	test_lexer(fd, "echo a | cat | cat | cat | cat | cat");
+	test_lexer(fd, "echo a >a >b >c >d >e >f >g");
+	test_lexer(fd, "");
+	test_lexer(fd, "213123 1231231 111 3332  123123");
+	test_lexer(fd, "echo > \"Hello, world!\"");
+	test_lexer(fd, "		   	 	 	 	   	\t \n \t \n							");
+	test_lexer(fd, "cat");
+
+	//Non valid tests.
 	test_lexer(fd, "echo \"Hello, world!");		//syntax error: unexpected end of file
 	test_lexer(fd, "echo \"Hello, world\")");	//syntax error near unexpected token ')'
 	test_lexer(fd, "ls >");						//syntax error near unexpected token 'newline'
