d6df06ead71873b57ddc5118ef291067ef54b6cb
Santi Guzman <sguzman@car2s4.42barcelona.com>
2024-04-09 19:42:04 +0200

    🍱 Here documents

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/23 19:30:33 by sguzman           #+#    #+#             */
-/*   Updated: 2024/04/09 13:29:47 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/04/09 19:37:20 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,7 @@ t_command	*parse_command(char *token)
 	arr = ft_split(token, ' ');
 	while (*arr)
 		words = make_word_list(*arr++, words);
-	redirects = make_redirection("test.txt", r_output_direction, NULL);
-	redirects = make_redirection("che.txt", r_output_direction, redirects);
+	redirects = make_redirection("test.txt", r_reading_until, NULL);
 	command = make_simple_command(words, redirects);
 	return (command);
 }
--- src/redir.c
+++ src/redir.c
@@ -6,11 +6,38 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/07 17:35:50 by sguzman           #+#    #+#             */
-/*   Updated: 2024/04/09 13:49:48 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/04/09 19:40:25 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static int	heredoc_write(int fd)
+{
+	size_t	nw;
+
+	nw = write(fd, "here_doc\n", 9);
+	if (nw < 0)
+		return (nw);
+	return (0);
+}
+
+static int	here_document_to_fd(void)
+{
+	int	r;
+	int	herepipe[2];
+
+	if (pipe(herepipe) < 0)
+		return (-1);
+	r = heredoc_write(herepipe[1]);
+	if (r)
+	{
+		close(herepipe[0]);
+		return (-1);
+	}
+	close(herepipe[1]);
+	return (herepipe[0]);
+}
 
 static int	do_redirection_internal(t_redirect *redirect)
 {
@@ -18,19 +45,19 @@ static int	do_redirection_internal(t_redirect *redirect)
 	enum e_instruction	ri;
 
 	ri = redirect->instruction;
-	if (ri != r_reading_until)
-	{
+	if (ri == r_reading_until)
+		fd = here_document_to_fd();
+	else
 		fd = open(redirect->filename, redirect->flags, 0666);
-		if (fd < 0)
-			return (errno);
-		if ((fd != redirect->dest) && (dup2(fd, redirect->dest) < 0))
-		{
-			close(fd);
-			return (errno);
-		}
-		if (fd != redirect->dest)
-			close(fd);
+	if (fd < 0)
+		return (errno);
+	if ((fd != redirect->dest) && (dup2(fd, redirect->dest) < 0))
+	{
+		close(fd);
+		return (errno);
 	}
+	if (fd != redirect->dest)
+		close(fd);
 	return (0);
 }
 
