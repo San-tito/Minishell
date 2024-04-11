ff7b207601a5a387e5338d3e3c5db98821100c9d
Santi Guzman <sguzman@car3s5.42barcelona.com>
2024-04-11 15:15:42 +0200

    💥 Builtin

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 18:41:45 by sguzman           #+#    #+#             */
-/*   Updated: 2024/04/10 23:29:32 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/04/11 15:15:26 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,16 @@
 int	echo(t_word_list *list)
 {
 	char	*temp;
-	int		i;
 	int		display_nl;
 
 	display_nl = 1;
 	while (list && *list->word == '-')
 	{
-		temp = list->word + 1;
-		i = 0;
-		while (temp[i])
-		{
-			if (ft_strchr("n", temp[i]) == 0)
+		temp = list->word;
+		while (*temp++)
+			if (ft_strchr("n", *temp) == 0)
 				break ;
-			i++;
-		}
-		if (*temp == 0 || temp[i])
+		if (*temp || *(list->word + 1) == 0)
 			break ;
 		display_nl = 0;
 		list = list->next;
