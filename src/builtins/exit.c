f6eee6f0ddec38617dd09f5998ca20555c3cef55
Santi Guzman <sguzman@car1s1.42barcelona.com>
2024-05-15 16:35:35 +0200

    🛂 Exit

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: sguzman <sguzman@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/13 16:36:22 by sguzman           #+#    #+#             */
-/*   Updated: 2024/05/13 19:00:01 by sguzman          ###   ########.fr       */
+/*   Updated: 2024/05/15 16:34:28 by sguzman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,18 @@ static int	get_exitstat(t_word_list *list)
 	if (list && list->word[0] == '-' && list->word[1] == '-' && !list->word[2])
 		list = list->next;
 	if (list == 0)
-		return (g_last_exit_value);
+		exit(g_last_exit_value);
 	if (list->word == 0 || legal_number(list->word, &status) == 0)
 		return (internal_error("%s: %s: %s", "exit", list->word,
-				"numeric argument required"), EX_BADUSAGE);
-	return (status);
+				"numeric argument required"), exit(EX_BADUSAGE), EX_BADUSAGE);
+	if (list->next)
+		return (internal_error("%s: %s", "exit", "too many arguments"),
+			g_last_exit_value);
+	return (exit(status), status);
 }
 
 int	exit_builtin(t_word_list *list)
 {
-	int	exit_value;
-
 	ft_putendl_fd("exit", 2);
-	exit_value = get_exitstat(list);
-	g_last_exit_value = exit_value;
-	exit(exit_value);
+	return (g_last_exit_value = get_exitstat(list));
 }
