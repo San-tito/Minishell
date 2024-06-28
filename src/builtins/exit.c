a310b30dc7ac1bb6eeedf559445836bdacf758a1
San-tito <68120901+San-tito@users.noreply.github.com>
2024-06-28 13:07:32 +0200

    one leak

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -10,27 +10,41 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "clear_cmd.h"
 #include "execute_cmd.h"
 #include "minishell.h"
 
-static void	exitstat(t_word_list *list)
+static int	exitstat(t_word_list *list, ssize_t *status)
 {
-	ssize_t	status;
-
 	if (list && list->word[0] == '-' && list->word[1] == '-' && !list->word[2])
 		list = list->next;
 	if (list == 0)
-		sh_exit(g_last_exit_value);
-	if (list->word == 0 || legal_number(list->word, &status) == 0)
-		return (internal_error("%s: %s: %s", "exit", list->word,
-				"numeric argument required"), sh_exit(EX_BADUSAGE));
+	{
+		*status = g_last_exit_value;
+		return (0);
+	}
+	if (list->word == 0 || legal_number(list->word, status) == 0)
+	{
+		internal_error("%s: %s: %s", "exit", list->word,
+			"numeric argument required");
+		*status = EX_BADUSAGE;
+		return (0);
+	}
 	if (list->next)
-		return (internal_error("%s: %s", "exit", "too many arguments"));
-	sh_exit(status & 255);
+	{
+		internal_error("%s: %s", "exit", "too many arguments");
+		return (1);
+	}
+	*status = *status & 255;
+	return (0);
 }
 
 int	exit_builtin(t_word_list *list)
 {
+	ssize_t	status;
+
 	ft_putendl_fd("exit", 2);
-	return (exitstat(list), g_last_exit_value);
+	if (exitstat(list, &status) == 0)
+		sh_exit(status);
+	return (g_last_exit_value);
 }
--- src/sh_malloc2.c
+++ src/sh_malloc2.c
@@ -14,6 +14,7 @@
 
 void	sh_exit(int s)
 {
+	rl_clear_history();
 	vlist_clear(varlist());
 	exit(s);
 }
